d230b8bf4d72b184ceea0d026e34fa2c1b29af6c
julenmendieta <jmendieta@milaboratories.com>
2025-04-11 16:22:05 +0200

    Include CDR3 sequences for bulk and sc in old MiXCR and bulk in new

--- model/src/index.ts
+++ model/src/index.ts
@@ -127,9 +127,11 @@ export const model = BlockModel.create()
     const inputSpec = ctx.resultPool.getPColumnSpecByRef(ctx.args.inputAnchor);
     // Old MiXCR versions
     let clonotypingRunId = inputSpec?.domain?.['pl7.app/vdj/clonotypingRunId'];
+    let chain = inputSpec?.domain?.['pl7.app/vdj/chain'];
     // New MiXCR versions
     if (clonotypingRunId === undefined) {
       clonotypingRunId = inputSpec?.axesSpec[1]?.domain?.['pl7.app/vdj/clonotypingRunId'];
+      chain = inputSpec?.axesSpec[1]?.domain?.['pl7.app/vdj/chain'];
     }
     if (clonotypingRunId === undefined) return undefined;
     const pCols = ctx.resultPool.getAnchoredPColumns(
@@ -157,55 +159,92 @@ export const model = BlockModel.create()
             'pl7.app/vdj/isScore': 'true',
           },
         },
+        // CDR3 aa sequence bulk data new MiXCR
+        {
+          annotationPatterns: {
+            'pl7.app/label': 'CDR3 aa',
+          },
+          axes: [{
+            domain: {
+              'pl7.app/vdj/clonotypingRunId': clonotypingRunId,
+              'pl7.app/vdj/chain': chain ?? '',
+            },
+          }],
+        },
+        // CDR3 aa sequence bulk data old MiXCR
+        {
+          annotationPatterns: {
+            'pl7.app/label': 'CDR3 aa',
+          },
+          domain: {
+            'pl7.app/vdj/clonotypingRunId': clonotypingRunId,
+            'pl7.app/vdj/chain': chain ?? '',
+          },
+        },
+        // @TODO: Look only for the chains on which we ran clustering
+        // CDR3 aa sequence sc data new MiXCR
+        {
+          annotationPatterns: {
+            'pl7.app/label': 'CDR3 aa Primary',
+          },
+          axes: [{
+            domain: {
+              'pl7.app/vdj/clonotypingRunId': clonotypingRunId,
+              'pl7.app/vdj/receptor': inputSpec?.domain?.['pl7.app/vdj/receptor'] ?? '',
+            },
+          }],
+        },
+        // CDR3 aa sequence sc data old MiXCR
+        {
+          annotationPatterns: {
+            'pl7.app/label': 'CDR3 aa Primary',
+          },
+          domain: {
+            'pl7.app/vdj/clonotypingRunId': clonotypingRunId,
+            'pl7.app/vdj/receptor': inputSpec?.domain?.['pl7.app/vdj/receptor'] ?? '',
+          },
+        },
+        // scFc new version (length Pcolumns are empty)
+        // {
+        //   annotationPatterns: {
+        //     'pl7.app/label': 'CDR3 aa',
+        //   },
+        //   domain: {
+        //     'pl7.app/vdj/clonotypingRunId': clonotypingRunId,
+        //   },
+        //   axes: [{
+        //     domain: {
+        //       'pl7.app/vdj/receptor': inputSpec?.axesSpec[1]?.domain?.['pl7.app/vdj/receptor'] ?? '',
+        //     },
+        //   }],
+        // },
       ],
     );
 
     if (pCols === undefined) return undefined;
+
+    // Check and modify pColumn to solve compatibility issues between MiXCR versions and packages
+    // @TODO: Remove when new version specs get consensuated
+    for (const p of pCols) {
+      const runIdtemp = p.spec?.domain?.['pl7.app/vdj/clonotypingRunId'];
+      const chaintemp = p.spec?.domain?.['pl7.app/vdj/chain'];
+      if (p.spec?.axesSpec?.[0].domain) {
+        if (runIdtemp) {
+          p.spec.axesSpec[0].domain['pl7.app/vdj/clonotypingRunId'] = runIdtemp;
+        }
+        if (chaintemp) {
+          p.spec.axesSpec[0].domain['pl7.app/vdj/chain'] = chaintemp;
+        }
+      }
+    }
 
     const scoresTable = createPlDataTable(
       ctx,
       pCols,
       ctx.uiState.tableState,
-      { filters: ctx.uiState.filterModel?.filters ?? [] },
     );
 
     return { scoresTable, count: pCols.length };
-  })
-
-  .output('test', (ctx) => {
-    if (ctx.args.inputAnchor === undefined) return undefined;
-    const inputAnchorDomain = ctx.resultPool.getPColumnSpecByRef(ctx.args.inputAnchor)?.domain;
-    if (inputAnchorDomain === undefined) return undefined;
-    const pCols = ctx.resultPool.getAnchoredPColumns(
-      { main: ctx.args.inputAnchor },
-      [
-        // first column condition will take any PCol satisfying below specs that have TWO axes
-        {
-          axes: [{
-            domain: {
-              'pl7.app/vdj/clonotypingRunId': inputAnchorDomain['pl7.app/vdj/clonotypingRunId'],
-            },
-          }, {}],
-          annotations: {
-            'pl7.app/vdj/isScore': 'true',
-          },
-        },
-        // second column condition (OR logic) will take any PCol satisfying below specs that have ONE axis
-        {
-          axes: [{
-            domain: {
-              'pl7.app/vdj/clonotypingRunId': inputAnchorDomain['pl7.app/vdj/clonotypingRunId'],
-            },
-          }],
-          annotations: {
-            'pl7.app/vdj/isScore': 'true',
-          },
-        },
-      ],
-    );
-
-    if (pCols === undefined) return undefined;
-    return pCols;
   })
 
   .output('isRunning', (ctx) => ctx.outputs?.getIsReadyOrError() === false)
